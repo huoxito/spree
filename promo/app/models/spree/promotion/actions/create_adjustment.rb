c1786004c6559985c5e0dd1cb5e23ceff37d09a2
Washington Luiz <huoxito@gmail.com>
2012-12-20 03:04:33 -0300

    Trying to get a collection of concurrent adjustments in promo action
    So we can check which one has the highest adjustment and then decide if
    the promotion linked to the promo action can be eligible

--- promo/app/models/spree/promotion/actions/create_adjustment.rb
+++ promo/app/models/spree/promotion/actions/create_adjustment.rb
@@ -4,13 +4,21 @@ module Spree
       class CreateAdjustment < PromotionAction
         calculated_adjustments
 
-        delegate :eligible?, :to => :promotion
+        # delegate :eligible?, :to => :promotion
+        has_one :adjustment, :as => :originator, :dependent => :destroy
 
         before_validation :ensure_action_has_calculator
 
+        # The first return call here is useless, The order input is already checked on the
+        # Promotion class.
+        #
+        # Besides checking if the promotions is already associated with the order should be
+        # a responsability of the Promotion and not the PromotionAction.
+        # TODO Move this check to Spree::Promotion class
+        # 
+        # TODO Investigate why there is two calls for the order.update! in this method
         def perform(options = {})
           return unless order = options[:order]
-          # Nothing to do if the promotion is already associated with the order
           return if order.promotion_credit_exists?(promotion)
 
           order.adjustments.promotion.reload.clear
@@ -19,10 +27,21 @@ module Spree
           order.update!
         end
 
+        # def eligible?(order)
+        #   self.promotion.eligible? 
+        # end
+
         # override of CalculatedAdjustments#create_adjustment so promotional
         # adjustments are added all the time. They will get their eligability
         # set to false if the amount is 0
-        def create_adjustment(label, target, calculable, mandatory=false)
+        #
+        # TODO Does it make any sense at all to create adjustments with amount 0?
+        # It probably indicates that the promotion should not even be eligible in
+        # the first place
+        #
+        # TODO Why is it setting source here? It's not used anyhere. Orders are
+        # retrieved through :adjustable
+        def create_adjustment(label, target, calculable, mandatory = false)
           amount = compute_amount(calculable)
           params = { :amount => amount,
                     :source => calculable,
@@ -37,11 +56,22 @@ module Spree
           [(calculable.item_total + calculable.ship_total), super.to_f.abs].min * -1
         end
 
-        private
-        def ensure_action_has_calculator
-          return if self.calculator
-          self.calculator = Calculator::FlatPercentItemTotal.new
+        def collect_discounts_sharing_products
+          concurring_adjustments = order.adjustments - [self.adjustment]
+          concurring_adjustments.inject([]) do |amount, adjustment|
+            adjustment_products = adjustment.originator.promotion.products
+
+            if self.promotion.products.any? { |product| adjustment_products.include?(product) }
+              amount << adjustment.amount
+            end
+          end
         end
+
+        private
+          def ensure_action_has_calculator
+            return if self.calculator
+            self.calculator = Calculator::FlatPercentItemTotal.new
+          end
       end
     end
   end
