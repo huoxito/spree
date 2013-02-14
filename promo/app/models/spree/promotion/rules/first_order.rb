731f70d5d7fd9d649e0f3601a92bb9412f855290
Washington Luiz <huoxito@gmail.com>
2013-02-14 18:25:18 -0300

    Fixes first order promo rule
    It would be eligible on user first order. But once user completes the
    order any update would make that order not eligible anymore

--- promo/app/models/spree/promotion/rules/first_order.rb
+++ promo/app/models/spree/promotion/rules/first_order.rb
@@ -2,10 +2,22 @@ module Spree
   class Promotion
     module Rules
       class FirstOrder < PromotionRule
+        attr_reader :user
+
         def eligible?(order, options = {})
-          user = order.try(:user) || options[:user]
-          !!(user && user.orders.complete.count == 0)
+          @user = order.try(:user) || options[:user]
+
+          if completed_orders.blank?
+            !!(user && completed_orders.blank?)
+          else
+            completed_orders.first == order
+          end 
         end
+
+        private
+          def completed_orders
+            user ? user.orders.complete : []
+          end
       end
     end
   end
--- promo/spec/models/promotion/rules/first_order_spec.rb
+++ promo/spec/models/promotion/rules/first_order_spec.rb
@@ -3,16 +3,15 @@ require 'spec_helper'
 describe Spree::Promotion::Rules::FirstOrder do
   let(:rule) { Spree::Promotion::Rules::FirstOrder.new }
   let(:order) { mock_model(Spree::Order, :user => nil) }
+  let(:user) { mock_model(Spree::LegacyUser) }
 
   it "should not be eligible without a user" do
     rule.should_not be_eligible(order)
   end
 
   context "should be eligible if user does not have any other completed orders yet" do
-    let(:user) { mock_model(Spree::LegacyUser) }
-
-    before do
-      user.stub_chain(:orders, :complete, :count => 0)
+    before(:each) do
+      user.stub_chain(:orders, :complete => [])
     end
 
     it "for an order without a user, but with user in payload data" do
@@ -25,11 +24,19 @@ describe Spree::Promotion::Rules::FirstOrder do
     end
   end
 
-  it "should be not eligible if user have at least one complete order" do
-    user = mock_model(Spree::LegacyUser)
-    user.stub_chain(:orders, :complete, :count => 1)
-    order.stub(:user => user)
+  context "user already completed an order" do
+    before(:each) do
+      order.stub(:user => user)
+    end
 
-    rule.should_not be_eligible(order)
+    it "any other order should not be eligible" do
+      user.stub_chain(:orders, :complete => [mock_model(Spree::Order)])
+      rule.should_not be_eligible(order)
+    end
+
+    it "should be eligible if checking against the same completed order" do
+      user.stub_chain(:orders, :complete => [order])
+      rule.should be_eligible(order)
+    end
   end
 end
