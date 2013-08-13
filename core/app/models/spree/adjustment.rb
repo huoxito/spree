62d95b42c99f91618645d51daf42b7af67ce7e16
Ryan Bigg <radarlistener@gmail.com>
2013-08-14 09:49:09 +1000

    No need to call reload any more in update! on adjustment

--- core/app/models/spree/adjustment.rb
+++ core/app/models/spree/adjustment.rb
@@ -68,10 +68,6 @@ module Spree
     # record from db for the association
     def update!
       return if immutable?
-      # Fix for #3381
-      # If we attempt to call 'source' before the reload, then source is currently
-      # the order object. After calling a reload, the source is the Shipment.
-      reload
       self.update_column(:amount, source.compute_amount(adjustable))
       # set_eligibility
     end
