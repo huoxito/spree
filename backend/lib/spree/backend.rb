0fb5011fac14d26c77b4361854ffafc793e67d8d
Washington Luiz <huoxito@gmail.com>
2014-02-10 23:39:49 -0300

    Backend depends on spree_api
    Only reason I can think of why not doing this is to avoid some
    duplicated routes trouble

--- backend/lib/spree/backend.rb
+++ backend/lib/spree/backend.rb
@@ -5,7 +5,8 @@ require 'jquery-ui-rails'
 require 'deface'
 require 'select2-rails'
 
-require 'spree/core'
+require 'spree_core'
+require 'spree_api'
 
 require 'spree/core/mail_settings'
 require 'spree/core/mail_interceptor'
